9b4520ba64d3e541c4efd383186792ed36db1dca
Ben-McDaniel <Benjamin.McDaniel123@gmail.com>
2025-02-09 17:05:20 -0500

    first build stuff

---
+++ build/CMakeFiles/flash_led.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for flash_led.
+# This may be replaced when dependencies are built.
---
+++ build/_deps/picotool-build/data_locs.cpp
@@ -0,0 +1,7 @@
+
+#include <vector>
+#include <string>
+
+std::vector<std::string> data_locs = {
+    "./","/home/mcdanibj/SupaCap_Software/build/_deps/picotool/"
+};
